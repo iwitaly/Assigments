f2c51d309dc367d5ed3fae2bfd77f66a03508d32
Виталий Давыдов <iwitaly@me.com>
2013-03-17 01:23:56 +0400

    набросок последнего дз

--- Assigment-4/dataCount/dataCount/main.c
+++ Assigment-4/dataCount/dataCount/main.c
@@ -7,12 +7,60 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+
+int *adr;
+
+void handler(int npid) {
+    
+    write(STDOUT_FILENO, "WOW!\n", 5);
+    
+    int check[1000000];
+    read(adr[0], check, 10000);
+    
+    
+    char str[5];
+    sprintf(str, "%ld", sizeof(*check));
+    write(STDOUT_FILENO, str, strlen(str));
+}
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    int *ppe = (int *)malloc(sizeof(100e6));
+    int ind = pipe(ppe);
+    adr = ppe;
+    
+    
+    pid_t pid = fork();
+    
+    if (pid == -1) {
+        write(STDOUT_FILENO, "shit happends", strlen("shit happends"));
+    }
+    if (!fork()) {
+        //child
+        while (1) {
+            int *trash = malloc(sizeof(int)*1e3);
+            write(ppe[1], trash, sizeof(int)*1e3);
+        }
+    }
+    else {
+        //parent
+        alarm(2);
+        
+        struct sigaction action;
+        action.sa_handler = handler;
+        
+        sigaction(SIGALRM, &action, NULL);
+        
+        int status;
+        wait(&status);
+    }
+    
     return 0;
+     
 }
 
